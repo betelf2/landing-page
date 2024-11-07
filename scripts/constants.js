8dad6fb2af0c480855d0cfaa0b5bbffd235fb2d0
Diego Reis <diegoluis200302@gmail.com>
2024-11-07 16:25:20 -0300

    criando arquivos de constantes

---
+++ scripts/constants.js
@@ -0,0 +1,21 @@
+export const DEPARTAMENTOS =
+{ 
+    INFANTIL: 'DEIBB'.toUpperCase(), 
+    ADOLESCENTES: 'TEENS'.toUpperCase(), 
+    JOVENS: 'ELO'.toUpperCase(), 
+    HOMENS: 'DEHOBB'.toUpperCase(), 
+    MULHERES: 'DEMUBB'.toUpperCase()
+};
+
+export const MINISTERIOS =
+{ 
+    MISSOES: 'MISSOES'.toUpperCase(), 
+    SOCIAL: 'SOCIAL'.toUpperCase(), 
+    INFANTIL: 'INFANTIL'.toUpperCase(), 
+    LOUVOR: 'LOUVOR'.toUpperCase(), 
+    DANCA: 'DANCA'.toUpperCase(), 
+    TEATRO: 'TEATRO'.toUpperCase(), 
+    COMUNICACAO: 'COMUNICACAO'.toUpperCase(), 
+    INTERCESSAO: 'INTERCESSAO'.toUpperCase(), 
+    CASAIS: 'CASAIS'.toUpperCase()
+};
