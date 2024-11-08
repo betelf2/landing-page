8ea23ac025d90c984e64430554b2ece9a537898a
diegolrs <diegosilva@eng.ci.ufpb.br>
2024-11-08 11:19:39 -0300

    consertando carrossel de gifs

--- scripts/carousel-of-gifs.js
+++ scripts/carousel-of-gifs.js
@@ -12,9 +12,11 @@ for(let i = 0; i < quantityOfItems; i++)
     // TODO:
     // adicionar estrutura html relacionada aos projetos e suas imagens
     var data = `<div class="${carouselItemClass}-${i}">
-                    <img src="images/slider/${0}.jpg" class="d-block w-100 gif-image" alt="Imagem ${i+1}"></img>
+                    <style>.${carouselItemClass}-${i} { position: relative !important; }</style>
+                    <img src="images/slider/${0}.jpg" style="position: relative;" class="gif-image gif-image-enabled" alt="Imagem ${i+1}"></img>
                     <img src="images/slider/${1}.jpg" class="d-block w-100 gif-image" alt="Imagem ${i+1}"></img>
                     <img src="images/slider/${2}.jpg" class="d-block w-100 gif-image" alt="Imagem ${i+1}"></img>
+                    <img src="images/slider/${3}.jpg" class="d-block w-100 gif-image" alt="Imagem ${i+1}"></img>
                 </div>
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Projeto ${i}</h5>
